4ad40001f77fe848532c7dfd019d3eb10b2e7dd1
Lexielist <48461313+Lexielist@users.noreply.github.com>
2019-04-08 16:19:13 -0400

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,25 @@
+
+
+x= 2
+y = 2.2
+print (x*y)
+
+months =('Jan','Feb', 'Mar', 'Apr', 'May', 'June','July')
+for month in months:
+    print (len (month))
+for month in months:
+    print (month)
+
+
+
+
+movie ={'Summer':'Titanic','Winter':'Jack and Jill','Autum':'Autum in NY'}
+for name,movie in movie.items():
+    print ('My favorite ' + name+' is '+ movie)
+
+def divide(a,b):
+    return print (a/b)
+divide (x,y)
+
+
+    
