708bdda13d93582ba778dfdd0ddf1f6677cf2228
Lexielist <48461313+Lexielist@users.noreply.github.com>
2019-04-10 14:33:29 -0400

    Add files via upload

---
+++ Eliza.py
@@ -0,0 +1,10 @@
+asking = True
+while asking:
+	answer = input("How are you? Please type how you feel or 'Q' to quit: ")
+	
+	if str.lower(answer) == "great" or str.lower(answer) == "q" or str.lower(answer == "good":
+		asking = False
+	else:
+		print("That's not what I'm asking for...")
+
+print("Thank you")
